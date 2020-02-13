492d8c7c2386ab3212b3219163562255e7b573ac
HosH <hosh0425@gmail.com>
2020-02-12 20:17:32 -0500

    I wanna go forward by the idea I have got in the stackOverflow. Doing perspective Transformation on text bounding box.I did not used page boundaries choosen by user still

--- DistortionCorrection/DistortionCorrection.pro
+++ DistortionCorrection/DistortionCorrection.pro
@@ -9,13 +9,16 @@ CONFIG -= app_bundle
 # deprecated API in order to know how to port your code away from it.
 DEFINES += QT_DEPRECATED_WARNINGS
 
+INCLUDEPATH += /home/hossein/installation/opencv/include/opencv2
+LIBS += $(shell pkg-config opencv --libs)
 # You can also make your code fail to compile if it uses deprecated APIs.
 # In order to do so, uncomment the following line.
 # You can also select to disable deprecated APIs only up to a certain version of Qt.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
-        main.cpp
+        main.cpp \
+    testperspectivetransform.cpp
 
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
--- DistortionCorrection/main.cpp
+++ DistortionCorrection/main.cpp
@@ -1,8 +1,63 @@
-#include <QCoreApplication>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <vector>
+#include <QDebug>
+using namespace std;
+using namespace cv;
+double divid =0.2;
 
-int main(int argc, char *argv[])
+Point2d upperLeft(764.11350209834291*divid, 621.9136455665207*divid);
+Point2d upperRight(2624.4176420761373*divid, 519.85418332633435*divid);
+Point2d lowerLeft(713.21758771346913*divid, 3639.7445176981314*divid);
+Point2d lowerRight(2712.7031913854335*divid, 3671.4076277199497*divid);
+
+Point2d upperLeftText(764.11350209834291*divid+45, 621.9136455665207*divid+95);
+Point2d upperRightText(2624.4176420761373*divid-60, 519.85418332633435*divid+107);
+Point2d lowerLeftText(713.21758771346913*divid+52, 3639.7445176981314*divid-240);
+Point2d lowerRightText(2712.7031913854335*divid-60, 3671.4076277199497*divid-255);
+int main()
 {
-    QCoreApplication a(argc, argv);
+    Mat img = imread("/home/hossein/QtWs/Distortion/ImageDistortion/DistortionCorrection/images/original2.png",CV_LOAD_IMAGE_COLOR);
+    resize(img , img , Size() , divid , divid );
+//    drawMarker(img , upperRightText , Scalar(0,0,255) , MARKER_CROSS , 20 , 3 , 8);
+    line(img,upperLeft,upperRight,Scalar(0,0,255));
+    line(img,upperLeft,lowerLeft,Scalar(0,0,255));
+    line(img,lowerLeft,lowerRight,Scalar(0,0,255));
+    line(img,lowerRight,upperRight,Scalar(0,0,255));
 
-    return a.exec();
+//    line(img,upperLeftText,upperRightText,Scalar(0,255,255));
+//    line(img,upperLeftText,lowerLeftText,Scalar(0,255,255));
+//    line(img,lowerLeftText,lowerRightText,Scalar(0,255,255));
+//    line(img,lowerRightText,upperRightText,Scalar(0,255,255));
+    vector<Point2f> src;
+    src.push_back(upperLeftText);
+    src.push_back(upperRightText);
+    src.push_back(lowerLeftText);
+    src.push_back(lowerRightText);
+    // Somehow good
+    //    src.push_back(lowerLeft);
+    //    src.push_back(upperLeft);
+    //    src.push_back(lowerRight);
+    //    src.push_back(upperRight);
+
+    vector<Point2f> dest;
+    dest.push_back(Point2f(upperLeftText.x,upperLeftText.y));
+    dest.push_back(Point2f(upperRightText.x+20,upperRightText.y));
+    dest.push_back(Point2f(lowerLeftText.x,lowerLeftText.y));
+    dest.push_back(Point2f(lowerRightText.x,lowerRightText.y));
+
+    Mat persPective = getPerspectiveTransform(src , dest);
+    Mat result = Mat(img.size(),img.type());
+    warpPerspective(img  , result  , persPective, Size2f(img.cols,img.rows));
+
+
+
+    imshow("Original image",img);
+    imshow("Perspective transformed",result);
+    imwrite("/home/hossein/Desktop/res.png",result);
+
+    waitKey(0);
+
+    return 0;
 }
+
